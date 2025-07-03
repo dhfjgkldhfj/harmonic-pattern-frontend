ff30572e7e5dbef366500fe5fb5d62f467e72df9
dhfjgkldhfj <spaartaan.kwt@gmail.com>
2025-07-03 05:22:09 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://harmonic-pattern-backend.onrender.com";
+const BASE_URL = "https://harmonic-pattern-backend.onrender.com";  // غيّرها لعنوان باك اند الخاص بك
 
 export async function fetchPatterns(symbol, interval) {
   try {
